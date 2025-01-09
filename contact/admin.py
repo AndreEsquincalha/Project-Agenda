be0cce8da2e77f52d065b4a817387aa205059914
AndreEsquincalha <andregarci1215@gmail.com>
2025-01-09 16:06:53 -0300

    Criação e migrate da Classe/Tabela 'Category'(Com chave estrangeira), para associar com contatos

--- contact/admin.py
+++ contact/admin.py
@@ -9,3 +9,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page=10
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name'
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= 'id','name'
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+        #Função importante para fazer nos Models, para nomear e ficar
+    def __str__(self): 
+        return self.name
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=60, blank=True)
@@ -11,6 +22,9 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+
+
 
     #Função importante para fazer nos Models, para nomear e ficar
     def __str__(self): 
