a3389b7a39582bf1cda1ef8383478b73566c4e21
AndreEsquincalha <andregarci1215@gmail.com>
2025-01-13 22:57:32 -0300

    Adição do Update: Criação da URL dinamica entre Create e Update

--- contact/forms.py
+++ contact/forms.py
@@ -1,12 +1,19 @@
 import re
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 
 from . import models
 
 
 class ContactForm(forms.ModelForm):
   
+  def __init__(self, *args,**kwargs):
+    super().__init__(*args,**kwargs)
+
+
   first_name = forms.CharField(
     widget=forms.TextInput(
       attrs={
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,23 +3,27 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
-from contact.forms import ContactForm
 from contact.models import Contact
+from django.urls import reverse
 
+from contact.forms import ContactForm
 
 def create(request):
+  form_action = reverse('contact:create')
+
   if request.method == 'POST':
     form = ContactForm(request.POST)
 
     context ={
-      'form': form
+      'form': form,
+      'form_action': form_action,
     }
 
     if form.is_valid():
-      form.save()
-      return redirect('contact:create')
+      contact = form.save()
+      return redirect('contact:update', contact_id=contact.pk)
 
-
+    #daqui pra baixo só será lido caso haja algum erro de entrada no formulario
     return render(
         request,
         'contact/create.html',
@@ -27,7 +31,41 @@ def create(request):
     ) 
   
   context ={
-    'form': ContactForm()
+    'form': ContactForm(),
+    'form_action': form_action,
+  }
+  return render(
+      request,
+      'contact/create.html',
+      context
+  ) 
+
+def update(request, contact_id):
+  contact = get_object_or_404(Contact, pk=contact_id,show = True)
+  form_action = reverse('contact:update', args=(contact_id))
+
+  if request.method == 'POST':
+    form = ContactForm(request.POST, instance=contact)
+
+    context ={
+      'form': form,
+      'form_action': form_action,
+    }
+
+    if form.is_valid():
+      contact = form.save()
+      return redirect('contact:update', contact_id=contact.pk)
+
+    #daqui pra baixo só será lido caso haja algum erro de entrada no formulario
+    return render(
+        request,
+        'contact/create.html',
+        context
+    ) 
+  
+  context ={
+    'form': ContactForm(instance=contact),
+    'form_action': form_action,
   }
   return render(
       request,
